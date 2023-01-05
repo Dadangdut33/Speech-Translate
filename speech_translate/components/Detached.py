e34ba45c75d86672638543f3f1f1402c40147035
Dadangdut33 <57717531+Dadangdut33@users.noreply.github.com>
2023-01-05 12:56:39 +0700

    add right click menu to detached window for better ux

--- speech_translate/components/Detached.py
+++ speech_translate/components/Detached.py
@@ -5,6 +5,7 @@ from typing import Literal
 from speech_translate.Globals import fJson, gClass, app_icon
 from speech_translate.utils.Beep import beep
 from .Tooltip import CreateToolTip
+from .MBox import Mbox
 
 
 # Classes
@@ -26,23 +27,20 @@ class AbstractDetachedWindow:
         self.updateTb = False
         self.getTbVal = False
         self.currentOpacity = 1.0
-        self.always_on_top = False
-        self.tooltip_disabled = False
-        self.hidden_top = False
-        self.clickThrough = False
+        self.always_on_top = tk.IntVar()
+        self.tooltip_disabled = tk.IntVar()
+        self.hidden_top = tk.IntVar()
+        self.clickThrough = tk.IntVar()
         if winType == "tc":
             gClass.ex_tcw = self  # type: ignore
         elif winType == "tl":
             gClass.ex_tlw = self  # type: ignore
 
+        # ------------------ #
         # Top frame
         self.frame_1 = ttk.Frame(self.root)
         self.frame_1.pack(side=tk.TOP, fill=tk.BOTH, expand=True)
-        self.fTooltip = CreateToolTip(
-            self.frame_1,
-            "Commands:\n-Alt + scroll to change opacity\n-Alt + t to toggle top window (remove top bar)\n-Alt + s to toggle click through or transparent window\n-Alt + o to toggle always on top\n-Alt + c to copy text\n-Alt + x to toggle on/off this tooltip",
-            wrapLength=400,
-        )
+        self.fTooltip = CreateToolTip(self.frame_1, "Right click for interaction menu\n\nTips: You can drag the window by dragging from the label", wrapLength=400)
 
         self.labelText = tk.Label(
             self.frame_1,
@@ -53,20 +51,36 @@ class AbstractDetachedWindow:
             justify=tk.LEFT,
         )
         self.labelText.pack(side=tk.TOP)
-        self.labelText.config(text="Hello, good morning, this is a test. I want to see what happened to the detach window. Go to the detach window. Check check. Hello. Go to the detach window.")
 
+        self.menuDropdown = tk.Menu(self.root, tearoff=0)
+        self.menuDropdown.add_command(label="Copy", command=lambda: self.copy_tb_content(), accelerator="Alt + C")
+        self.menuDropdown.add_separator()
+        self.menuDropdown.add_checkbutton(label="Hide Title bar", command=lambda: self.toggle_hidden_top(False), onvalue=1, offvalue=0, variable=self.hidden_top, accelerator="Alt + T")
+        self.menuDropdown.add_checkbutton(label="Click Through/Transparent", command=lambda: self.toggle_click_through(False), onvalue=1, offvalue=0, variable=self.clickThrough, accelerator="Alt + S")
+        self.menuDropdown.add_checkbutton(label="Always On Top", command=lambda: self.toggle_always_on_top(False), onvalue=1, offvalue=0, variable=self.always_on_top, accelerator="Alt + O")
+        self.menuDropdown.add_separator()
+        self.menuDropdown.add_command(label="Increase Opacity by 0.1", command=lambda: self.increase_opacity(), accelerator="Mouse Wheel Up")
+        self.menuDropdown.add_command(label="Decrease Opacity by 0.1", command=lambda: self.decrease_opacity(), accelerator="Mouse Wheel Down")
+        self.menuDropdown.add_separator()
+        self.menuDropdown.add_checkbutton(label="Hide Tooltip", command=lambda: self.disable_tooltip(False), onvalue=1, offvalue=0, variable=self.tooltip_disabled, accelerator="Alt + X")
+        self.menuDropdown.add_separator()
+        self.menuDropdown.add_command(label="Keyboard Shortcut Keys", command=lambda: self.show_shortcut_keys())
+
+        # ------------------------------------------------------------------------
+        # Binds
         # On Close
         self.root.protocol("WM_DELETE_WINDOW", self.on_closing)
 
+        # rclick menu
+        self.root.bind("<Button-3>", lambda event: self.menuDropdown.post(event.x_root, event.y_root))
+
+        # keybinds
         self.root.bind("<Alt-KeyPress-s>", lambda event: self.toggle_click_through())
         self.root.bind("<Alt-KeyPress-c>", lambda event: self.copy_tb_content())
         self.root.bind("<Alt-KeyPress-t>", lambda event: self.toggle_hidden_top())
         self.root.bind("<Alt-KeyPress-o>", lambda event: self.toggle_always_on_top())
         self.root.bind("<Alt-KeyPress-x>", lambda event: self.disable_tooltip())
         self.root.bind("<Alt-MouseWheel>", lambda event: self.change_opacity(event))
-
-        # remove focus from textbox
-        self.root.bind("<FocusIn>", lambda event: self.root.focus_force())
 
         # bind resize
         self.frame_1.bind("<Configure>", lambda event: self.on_resize(event))
@@ -143,11 +157,26 @@ class AbstractDetachedWindow:
         if self.labelText.winfo_height() > self.frame_1.winfo_height():
             self.root.geometry(f"{self.root.winfo_width()}x{self.labelText.winfo_height()}")
 
+    def show_shortcut_keys(self):
+        """
+        Method to show shortcut keys.
+        """
+        Mbox(
+            "Shortcut keys command for detached window",
+            "Alt + scroll to change opacity\nAlt + c to copy text\nAlt + t to toggle title bar (remove title bar)\nAlt + s to toggle click through or transparent window\nAlt + o to toggle always on top\nAlt + x to toggle on/off this tooltip\n\nTips: You can drag the window by dragging from the label",
+            0,
+        )
+
     # disable tooltip
-    def disable_tooltip(self):
+    def disable_tooltip(self, fromKeyBind=True):
+        """
+        Method to toggle tooltip.
+        """
         beep()
-        self.tooltip_disabled = not self.tooltip_disabled
-        if self.tooltip_disabled:
+        if fromKeyBind:
+            self.tooltip_disabled.set(0 if self.tooltip_disabled.get() == 1 else 1)
+
+        if self.tooltip_disabled.get() == 1:
             self.fTooltip.hidetip()
             self.fTooltip.opacity = 0
         else:
@@ -155,19 +184,44 @@ class AbstractDetachedWindow:
             self.fTooltip.opacity = self.currentOpacity
 
     # show/hide top
-    def toggle_hidden_top(self):
+    def toggle_hidden_top(self, fromKeyBind=True):
+        """
+        Method to toggle hidden top.
+        """
         beep()
-        self.hidden_top = not self.hidden_top
-        self.root.overrideredirect(self.hidden_top)
+        if fromKeyBind:
+            self.hidden_top.set(0 if self.hidden_top.get() == 1 else 1)
 
-    # Stay on top
-    def toggle_always_on_top(self):
+        self.root.overrideredirect(True if self.hidden_top.get() == 1 else False)
+
+    def toggle_click_through(self, fromKeyBind=True):
+        """
+        Method to toggle click through.
+        """
         beep()
-        self.always_on_top = not self.always_on_top
-        self.root.wm_attributes("-topmost", self.always_on_top)
+        if fromKeyBind:
+            self.clickThrough.set(0 if self.clickThrough.get() == 1 else 1)
 
-    # Show/Hide
+        if self.clickThrough.get() == 1:
+            self.root.wm_attributes("-transparentcolor", self.root["bg"])
+        else:
+            self.root.wm_attributes("-transparentcolor", "")
+
+    def toggle_always_on_top(self, fromKeyBind=True):
+        """
+        Method to toggle always on top.
+        """
+
+        beep()
+        if fromKeyBind:
+            self.always_on_top.set(0 if self.always_on_top.get() == 1 else 1)
+
+        self.root.wm_attributes("-topmost", True if self.always_on_top.get() == 1 else False)
+
     def show(self):
+        """
+        Method to show the window.
+        """
         self.root.attributes("-alpha", 1)
         self.root.attributes("-transparentcolor", "")
         self.root.wm_deiconify()
@@ -175,8 +229,34 @@ class AbstractDetachedWindow:
     def on_closing(self):
         self.root.wm_withdraw()
 
+    def increase_opacity(self):
+        """
+        Method to increase the opacity of the window by 0.1.
+        """
+        self.currentOpacity += 0.1
+        if self.currentOpacity > 1:
+            self.currentOpacity = 1
+        self.root.attributes("-alpha", self.currentOpacity)
+        self.fTooltip.opacity = self.currentOpacity
+
+    def decrease_opacity(self):
+        """
+        Method to decrease the opacity of the window by 0.1.
+        """
+        self.currentOpacity -= 0.1
+        if self.currentOpacity < 0.1:
+            self.currentOpacity = 0.1
+        self.root.attributes("-alpha", self.currentOpacity)
+        self.fTooltip.opacity = self.currentOpacity
+
     # opacity change
     def change_opacity(self, event):
+        """
+        Method to change the opacity of the window by scrolling.
+
+        Args:
+            event (event): event object
+        """
         if event.delta > 0:
             self.currentOpacity += 0.1
         else:
@@ -190,13 +270,8 @@ class AbstractDetachedWindow:
         self.fTooltip.opacity = self.currentOpacity
 
     def copy_tb_content(self):
+        """
+        Method to copy the textbox content to clipboard.
+        """
         self.root.clipboard_clear()
         self.root.clipboard_append(self.labelText.cget("text").strip())
-
-    def toggle_click_through(self):
-        beep()
-        self.clickThrough = not self.clickThrough
-        if self.clickThrough:
-            self.root.wm_attributes("-transparentcolor", self.root["bg"])
-        else:
-            self.root.wm_attributes("-transparentcolor", "")
